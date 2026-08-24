ca5ecc1255b35bd31cfed84aab2c8978ce2d5cda
vin123vin123 <vin33666@gmail.com>
2026-08-24 15:28:37 +0530

    Create tailwind.config.js

---
+++ app/tailwind.config.js
@@ -0,0 +1,14 @@
+### File 2: `tailwind.config.js`
+```javascript
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
